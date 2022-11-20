ee37ee609003b8d18ebeccc6296efd0e8eb39d04
jamesole <87213195+jamesole@users.noreply.github.com>
2022-11-19 19:17:58 -0500

    Create index.js

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />
+);
